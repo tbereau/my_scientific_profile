3f0b1c10bd0c8f8b952b9f911ddc7cc0eb185c1e
Tristan Bereau <tristan.bereau@gmail.com>
2023-07-05 15:18:14 +0200

    Add pd dataframe functions

--- my_scientific_profile/database/authors.py
+++ my_scientific_profile/database/authors.py
@@ -1,4 +1,6 @@
 from typing import Any
+import pandas as pd
+from dataclasses import asdict
 
 from my_scientific_profile.authors.authors import Author
 from my_scientific_profile.database.utils import (
@@ -7,7 +9,11 @@ from my_scientific_profile.database.utils import (
 )
 from my_scientific_profile.papers.papers import fetch_all_paper_authors
 
-__all__ = ["save_dataclass_records_to_s3", "load_all_paper_authors_from_s3"]
+__all__ = [
+    "save_dataclass_records_to_s3",
+    "load_all_paper_authors_from_s3",
+    "convert_paper_authors_to_dataframe"
+]
 
 
 def save_all_paper_authors_to_s3(s3_client: Any, s3_bucket: str) -> None:
@@ -17,3 +23,7 @@ def save_all_paper_authors_to_s3(s3_client: Any, s3_bucket: str) -> None:
 
 def load_all_paper_authors_from_s3(s3_client: Any, s3_bucket: str) -> list[Author]:
     return read_dataclass_records_from_s3(Author, s3_client, s3_bucket)
+
+
+def convert_paper_authors_to_dataframe(authors: list[Author]) -> pd.DataFrame:
+    return pd.json_normalize([asdict(author) for author in authors])
--- my_scientific_profile/database/papers.py
+++ my_scientific_profile/database/papers.py
@@ -1,4 +1,6 @@
 from typing import Any
+import pandas as pd
+from dataclasses import asdict
 
 from my_scientific_profile.database.utils import (
     read_dataclass_records_from_s3,
@@ -10,6 +12,7 @@ __all__ = [
     "save_dataclass_records_to_s3",
     "load_all_papers_from_s3",
     "save_all_papers_to_s3",
+    "convert_papers_to_dataframe"
 ]
 
 
@@ -20,3 +23,7 @@ def save_all_papers_to_s3(s3_client: Any, s3_bucket: str) -> None:
 
 def load_all_papers_from_s3(s3_client: Any, s3_bucket: str) -> list[Paper]:
     return read_dataclass_records_from_s3(Paper, s3_client, s3_bucket)
+
+
+def convert_papers_to_dataframe(papers: list[Paper]) -> pd.DataFrame:
+    return pd.json_normalize([asdict(paper) for paper in papers])
